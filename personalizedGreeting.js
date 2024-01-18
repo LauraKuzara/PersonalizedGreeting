c51a6277e41c478253ef602b5f3c0d2452509c3a
LauraKuzara <kuzaralaura@gmail.com>
2024-01-17 20:47:24 -0500

    completed up to bonus questions

--- personalizedGreeting.js
+++ personalizedGreeting.js
@@ -1,9 +1,27 @@
-const firstName = "Laura";
+const firstName = "Shaun";
 
-let lastName = "Kuzara";
+let lastName = "Gillow";
 
 let myAge = 31;
 
 let myCity = "Gainesville";
 
 console.log(`Hello! My name is ${firstName} ${lastName}! I’m ${myAge} years old, and I’m from ${myCity}. It’s nice to meet you!`);
+
+let myInitials = firstName[0] +lastName[0];
+
+console.log(myInitials);
+
+let nameLength = firstName.length;
+
+console.log(nameLength);
+
+let myFullName = firstName + " " + lastName;
+
+console.log(myFullName.toUpperCase());
+
+const sayGreeting = (firstName, lastName, myAge, myCity) => {
+    return `Hello! My name is ${firstName} ${lastName}! I’m ${myAge} years old, and I’m from ${myCity}. It’s nice to meet you!`;
+}
+
+console.log(sayGreeting("Laura", "Kuzara", 31, "Gainesville"));

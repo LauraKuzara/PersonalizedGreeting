20a2feca348350afb04642938aa499fa1f2b9b8c
LauraKuzara <kuzaralaura@gmail.com>
2024-01-17 20:29:21 -0500

    completed up to step 5

---
+++ personalizedGreeting.js
@@ -0,0 +1,9 @@
+const firstName = "Laura";
+
+let lastName = "Kuzara";
+
+let myAge = 31;
+
+let myCity = "Gainesville";
+
+console.log(`Hello! My name is ${firstName} ${lastName}! I’m ${myAge} years old, and I’m from ${myCity}. It’s nice to meet you!`);
